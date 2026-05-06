0b22d603385a9e21999bb23379a075ab43dc6941
Rene Schwarzer <rene_schwarzer@hotmail.de>
2026-05-06 18:46:28 +0200

    feat: general improvements and minor bugs

---
+++ src/KleeneStar.Portal/KleeneStarPlugin.cs
@@ -0,0 +1,30 @@
+﻿using KleeneStar.Core;
+using WebExpress.WebCore;
+using WebExpress.WebCore.WebAttribute;
+using WebExpress.WebCore.WebPlugin;
+
+namespace KleeneStar.Portal
+{
+    [Name("kleenestar.core:plugin.name")]
+    [Description("kleenestar.core:plugin.description")]
+    [Icon("/assets/img/kleenestar.svg")]
+    [Application<KleeneStarApplication>()]
+    [Dependency("webexpress.webapp")]
+    public sealed class KleeneStarPlugin : IPlugin
+    {
+        /// <summary>  
+        /// Initializes a new instance of the class.  
+        /// </summary>  
+        public KleeneStarPlugin()
+        {
+            WebEx.Favicon = "/assets/img/kleenestar.ico";
+        }
+
+        /// <summary>  
+        /// Called when the plugin starts working. Run is called concurrently.  
+        /// </summary>  
+        public void Run()
+        {
+        }
+    }
+}
