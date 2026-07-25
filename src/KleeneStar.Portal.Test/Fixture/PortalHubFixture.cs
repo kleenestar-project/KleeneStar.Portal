a5e5db81fc210fdd76838536e958a421f39d37a3
René Schwarzer <rene_schwarzer@hotmail.de>
2026-07-25 09:22:46 +0200

    feat: kanban and dashboard improvements

--- src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
+++ src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
@@ -29,6 +29,8 @@ namespace KleeneStar.Portal.Test
             ("_statusManager",    typeof(StatusManager)),
             ("_objectManager",    typeof(ObjectManager)),
             ("_dashboardManager", typeof(DashboardManager)),
+            ("_kanbanBoardManager", typeof(KanbanBoardManager)),
+            ("_kindDashboardManager", typeof(KindDashboardManager)),
             ("_tenantManager",    typeof(TenantManager)),
             ("_identityManager",  typeof(IdentityManager)),
             ("_groupManager",     typeof(GroupManager)),
