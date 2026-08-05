9a1320a1b73be16f1bd997e4add39a0a1b766f76
René Schwarzer <rene_schwarzer@hotmail.de>
2026-08-05 22:24:47 +0200

    feat: general improvements and minor bugs

--- src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
+++ src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
@@ -33,6 +33,8 @@ namespace KleeneStar.Portal.Test
             ("_kindDashboardManager", typeof(KindDashboardManager)),
             ("_tenantManager",    typeof(TenantManager)),
             ("_navigatorLinkManager", typeof(NavigatorLinkManager)),
+            ("_maintenanceManager", typeof(MaintenanceManager)),
+            ("_customQuickfilterManager", typeof(CustomQuickfilterManager)),
             ("_identityManager",  typeof(IdentityManager)),
             ("_groupManager",     typeof(GroupManager)),
             ("_slaManager",       typeof(SlaManager)),
