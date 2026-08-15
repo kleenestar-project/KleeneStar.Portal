612c150c022d38e252b2f76bbca1d64270657eb3
René Schwarzer <rene_schwarzer@hotmail.de>
2026-08-15 17:35:54 +0200

    feat: add profile pages and minor bugs

--- src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
+++ src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
@@ -50,6 +50,8 @@ namespace KleeneStar.Portal.Test
             ("_objectViewManager",typeof(ObjectViewManager)),
             ("_objectLinkManager",typeof(ObjectLinkManager)),
             ("_sessionManager",   typeof(SessionManager)),
+            ("_identitySessionManager", typeof(IdentitySessionManager)),
+            ("_accessTokenManager", typeof(AccessTokenManager)),
             ("_savedSearchManager",typeof(SavedSearchManager)),
             ("_sprintManager",    typeof(SprintManager)),
         ];
