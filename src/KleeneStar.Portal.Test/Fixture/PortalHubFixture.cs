5dad995c6a48254849fcd91ee10fd1e08ad2c4a7
René Schwarzer <rene_schwarzer@hotmail.de>
2026-09-05 16:56:13 +0200

    feat: add security level, general improvements and minor bugs

--- src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
+++ src/KleeneStar.Portal.Test/Fixture/PortalHubFixture.cs
@@ -24,6 +24,7 @@ namespace KleeneStar.Portal.Test
             ("_workspaceTemplateManager", typeof(WorkspaceTemplateManager)),
             ("_classManager",     typeof(ClassManager)),
             ("_fieldManager",     typeof(FieldManager)),
+            ("_securityLevelManager", typeof(SecurityLevelManager)),
             ("_formManager",      typeof(FormManager)),
             ("_priorityManager",  typeof(PriorityManager)),
             ("_workflowManager",  typeof(WorkflowManager)),
